404b16c6c579b72b71de4a07b669d42afd8c4ec6
Saugat Rimal <saugatrimal60@gmail.com>
2022-12-26 15:35:10 +0545

    made some changes but still not completed

--- src/components/context.js
+++ src/components/context.js
@@ -1,8 +1,36 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
+import reducer from "./reducer";
 
+let API = "https://hn.algolia.com/api/v1/search?";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const initialState = {
+    query: " react",
+    nbPages: 0,
+    page: 0,
+    hits: [],
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchApi = async (url) => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      // isLoading = false;
+
+      // console.log(result);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi(`{API} query={state.query}&page={state.page} `);
+  }, []);
+
   return <AppContext.Provider value={"saugat"}>{children}</AppContext.Provider>;
 };
 
